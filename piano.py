89700a7a68e330605b12393b54956f3e112428d5
robertvorthman <robertvorthman@users.noreply.github.com>
2019-10-18 14:21:49 -0400

    Changed midi synthesizer to mingus.midi.pyfluidsynth

--- piano.py
+++ piano.py
@@ -5,25 +5,31 @@ from neopixel import * #colored lights
 #midi player libraries
 from mingus.core import notes, chords 
 from mingus.containers import * 
-from mingus.midi import fluidsynth
+import mingus.midi.pyfluidsynth as fluidsynth
 import RPi.GPIO as GPIO #for GPIO switch
 
-#fluidsynth.init('/usr/share/sounds/sf2/FluidR3_GM.sf2',"alsa")
-fluidsynth.init('/home/pi/Documents/Halloween Piano/soundFonts/Arachno SoundFont - Version 1.0.sf2',"alsa") #custom sound font sounds slightly better than FluidR3_GM.sf2, but file size is larger
+synth = fluidsynth.Synth(1)
+synth.start(driver="alsa")
+
+# soundFont = synth.sfload("/home/pi/Documents/ChoriumRevA.SF2")
+soundFont = synth.sfload("/home/pi/Documents/Arachno SoundFont - Version 1.0.sf2")
+
 
 #assign instruments to separate channel numbers (1 is hammond organ, 2 is steel guitar, etc...)
-fluidsynth.set_instrument(1, 16) #16 hammond organ 
-fluidsynth.set_instrument(2, 26) #26 steel guitar
-fluidsynth.set_instrument(3, 0, 128) #0 Bank 128 standard drums
-fluidsynth.set_instrument(4, 114) #114 steel drums
-fluidsynth.set_instrument(5, 48, 128) #48 bank 128 orchestra drum kit
-fluidsynth.set_instrument(6, 25, 128) #25 bank 128 TR-808 drum kit
-fluidsynth.set_instrument(7, 102) #102 echo drops
-fluidsynth.set_instrument(8, 11) #11 vibraphone
-fluidsynth.set_instrument(9, 13) #13 xylophone
-fluidsynth.set_instrument(10, 55) #55 orchestra hit
-fluidsynth.set_instrument(11, 86) #5th saw wave
-fluidsynth.set_instrument(12, 88) #fantasia
+#program_select arguments: channel, soundFont, bank, preset
+synth.program_select(1, soundFont, 0, 16) #16 hammond organ 
+synth.program_select(2, soundFont, 0, 26) #26 steel guitar
+synth.program_select(3, soundFont, 128, 0) #0 Bank 128 standard drums
+synth.program_select(4, soundFont, 0, 114) #114 steel drums
+synth.program_select(5, soundFont, 128, 48) #48 bank 128 orchestra drum kit
+synth.program_select(6, soundFont, 128, 25) #25 bank 128 TR-808 drum kit
+synth.program_select(7, soundFont, 0, 102) #102 echo drops
+synth.program_select(8, soundFont, 0, 11) #11 vibraphone
+synth.program_select(9, soundFont, 0, 13) #13 xylophone
+synth.program_select(10, soundFont, 0, 55) #55 orchestra hit
+synth.program_select(11, soundFont, 0, 86) #86 5th saw wave
+synth.program_select(12, soundFont, 0, 88) #88 fantasia
+
 #instrument numbers: https://www.midi.org/specifications/item/gm-level-1-sound-set
 #Use Polysynth desktop application to open sf2 files and find instrument and bank numbers
 
@@ -31,7 +37,7 @@ fluidsynth.set_instrument(12, 88) #fantasia
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN)
 modeButtonLastPushed = time.time() 
-print "Push Button Ready"
+print ("Push Button Ready")
 
 #each mode has a name which is printed to the python terminal output, each mode plays different sounds
 modes = ['96tears', 'steelDrum', 'drumKit', 'techno', 'echoDrops', 'vibraphone', 'xylophone', 'orchestraHit', 'fifthSawWave', 'fantasia']
@@ -52,11 +58,11 @@ notes = [['','','','','D-6', 'D#-6', 'E-6', 'G-6', 'A#-7', 'B-7', 'C-7', 'D-7'],
 modeChannels = [1, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 #define special notes and chords for 96tears modes
-drumBass = Note('C', 2)
-drumSnare = Note('E', 2)
-chordG = NoteContainer(['G-2', 'B-3', 'D-3'])
-chordC7 = NoteContainer(['C-3', 'E-3', 'G-3', 'Bb-3'])
-print "Sounds Ready."
+drumBass = 36
+drumSnare = 40
+chordG = NoteContainer(['G-3', 'B-4', 'D-4'])
+chordC7 = NoteContainer(['C-4', 'E-4', 'G-4', 'Bb-4'])
+print ("Sounds Ready.")
 
 # LED strip configuration:
 LED_COUNT      = 50      # Number of LED pixels.
@@ -77,40 +83,53 @@ lightSegments = [[0,4], [6,10], [12,14], [16,18], [19,22], [24,26], [27,30], [31
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 # Intialize the library (must be called once before other functions).
 strip.begin()
-print "Lights Ready."
+print ("Lights Ready.")
 
 
 
 #initialize 12-key capacitive switch sensor
 cap = MPR121.MPR121()
 if not cap.begin():
-    print 'Error initializing MPR121.  Check your wiring!'
+    print ('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
 
 #plays note and turns on neopixels
 def activateKey(key):
+    print ('Activate key {0} on mode {1}'.format(key, currentMode))
     if currentMode > 0 or key > 3:
-        fluidsynth.play_Note(Note(notes[currentMode][key]), modeChannels[currentMode], 70)
-        print notes[currentMode][key]
+        synth.noteon(modeChannels[currentMode], int(Note(notes[currentMode][key])), 90)
+        print (notes[currentMode][key])
     #the 96tears mode is unlike other modes in that the first two switches are drums and the next two switches play chords
     elif key == 0:
-        fluidsynth.play_Note(drumSnare, 3, 90)
-        print "Snare"  
+        synth.noteon(3, drumSnare, 90)
+        print ("Snare")  
     elif key == 1:
-        fluidsynth.play_Note(drumBass, 3, 127)   
-        print "Bass"
+        synth.noteon(3, drumBass, 90)
+        print ("Bass")
     elif key == 2:
-        fluidsynth.play_NoteContainer(chordC7, 2, 60)    
-        print "Chord C7"  
+        print ("Chord C7")
+        for note in chordC7.notes:
+            print (int(Note(note)))
+            if int(Note(note)) == 52:
+                synth.noteon(2, int(Note(note)), 60)
+            else:
+                synth.noteon(2, int(Note(note)), 40)
+        
     elif key == 3:
-        fluidsynth.play_NoteContainer(chordG, 2, 60)
-        print "Chord G"
+        print ("Chord G")
+        for note in chordG.notes:
+            print (int(Note(note)))
+            if int(Note(note)) == 43:
+                synth.noteon(2, int(Note(note)), 60)
+            else:
+                synth.noteon(2, int(Note(note)), 40)
+        
     
     #calculate neopixel color, each key is a different shade of the rainbow
     positionProduct = key*255
     notesLength = len(notes[currentMode])
     wheelPosition = positionProduct/notesLength
-    #print "Color:", wheelPosition
+    #print ("Color:", wheelPosition)
     for i in range(strip.numPixels()):
         if i >= lightSegments[key][0] and i <= lightSegments[key][1]:
             strip.setPixelColor(i, wheel(wheelPosition))
@@ -119,16 +138,17 @@ def activateKey(key):
 #stops note and turns off neopixels
 def deactivateKey(key):
     if currentMode > 0 or key > 3:
-        fluidsynth.stop_Note(Note(notes[currentMode][key]), modeChannels[currentMode])
+        synth.noteoff(modeChannels[currentMode], int(Note(notes[currentMode][key])))
     elif key == 0:
-        fluidsynth.stop_Note(drumSnare, 3)
+        synth.noteoff(3, drumSnare)
     elif key == 1:
-        fluidsynth.stop_Note(drumBass, 3)    
+        synth.noteoff(3, drumBass)
     elif key == 2:
-        fluidsynth.stop_NoteContainer(chordC7, 2)
+        for note in chordC7.notes:
+            synth.noteoff(2, int(Note(note)))
     elif key == 3:
-        fluidsynth.stop_NoteContainer(chordG, 2)
-    
+        for note in chordG.notes:
+            synth.noteoff(2, int(Note(note)))    
     for i in range(strip.numPixels()):
         if i >= lightSegments[key][0] and i <= lightSegments[key][1]:
             strip.setPixelColor(i, Color(0,0,0))
@@ -156,7 +176,7 @@ def changeMode(channel):
         temp = currentMode + 1
         if (currentMode + 1) > len(modes):
             currentMode -= len(modes)
-        print "Change Mode", modes[currentMode]
+        print ("Change Mode", modes[currentMode])
     modeButtonLastPushed = now
 
 def stopAllNotes():
@@ -164,7 +184,7 @@ def stopAllNotes():
         deactivateKey(i)
 
 last_touched = cap.touched()
-print "Switches Ready."
+print ("Switches Ready.")
 
 #when GPIO button on pin 20 is pressed (voltage rises from low to high), change mode
 GPIO.add_event_detect(20, GPIO.RISING, callback=changeMode) 
@@ -189,11 +209,11 @@ while True:
             pin_bit = 1 << i
             # First check if transitioned from not touched to touched.
             if current_touched & pin_bit and not last_touched & pin_bit:
-                #print '{0} touched!'.format(i)
+                #print ('{0} touched!'.format(i))
                 activateKey(i)
             # Next check if transitioned from touched to not touched.
             if not current_touched & pin_bit and last_touched & pin_bit:
-                #print '{0} released!'.format(i)
+                #print ('{0} released!'.format(i))
                 deactivateKey(i)
         # Update last state and wait a short period before repeating.
         last_touched = current_touched
